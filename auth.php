1af5e0499ab319f6ac05fa951a073035d7f5769c
Joshua Reyes <121803798+CIVYER@users.noreply.github.com>
2023-10-21 10:06:30 +0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,8 +1,8 @@
 <?php
     header("Access-Control-Allow-Origin: *");  // Allow cross-origin requests
 
-    $email = "joshuavreyes111222@gmail.com";
-    // $email = $_POST['email'];
+    // $email = "joshuavreyes111222@gmail.com";
+    $email = $_POST['email'];
 
     $command = escapeshellcmd("python ./auth.py $email");
     $authCode = shell_exec($command);
